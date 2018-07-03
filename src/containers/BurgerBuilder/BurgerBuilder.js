8f1fc62e9589afe164603133c520998c2665e76c
ion utale <ion.utale@icloud.com>
2018-07-03 15:08:40 +0200

    added axios implemented : save order top the firebase db using post method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../Axios-orders'
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -79,7 +79,28 @@ class BugerBuilder extends Component {
     }
 
     purchanseContinueHandler = () => {
-        alert('you continued')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.price,
+            customer: {
+                name: 'Ion',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '21435',
+                    country: 'italy'
+                },
+                email:'test@test.com'
+            },
+            deliveryMode: 'fastest'
+        }
+
+        axios.post('/orders.json', {order})
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 
     render () {
