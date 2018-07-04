0de16d5dd8e9aa11a4ecf897290a2c5326da7a68
ion utale <ion.utale@icloud.com>
2018-07-04 16:38:39 +0200

    USER INPUT analizing the app creating a Custom Dynamic Input Component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import classes from './Input.css'
+const Input = (props) => {
+    let inputElement = null;
+    switch (props.inputtype) {
+        case('input'):
+            inputElement = <input className={classes.InputElement} {...props}/>
+        case('textArea'):
+            inputElement = <textarea className={classes.InputElement} {...props}/>
+
+        default: 
+        inputElement = <input className={classes.InputElement} {...props}/>
+    }
+   return (
+    <div className={classes.Input}>
+        <label className={classes.Label}>{props.label}</label>
+        {inputElement}   
+ </div>
+    )   
+}
+
+export default Input
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import axios from '../../../Axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -47,11 +48,12 @@ class ContactData extends Component {
     }
 
     render() {
-        let form = (<form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-            <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-            <input className={classes.Input} type="text" name="street" placeholder="Your street" />
-            <input className={classes.Input} type="text" name="postalCode" placeholder="Your postalCode" />
+        let form = (
+        <form>
+            <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+            <Input inputtype="input" type="email" name="email" placeholder="Your email" />
+            <Input inputtype="input" type="text" name="street" placeholder="Your street" />
+            <Input inputtype="input" type="text" name="postalCode" placeholder="Your postalCode" />
             <Button 
             btnType="Success"
             clicked={this.orderHnadler}
